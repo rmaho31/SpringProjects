d8e225a944c671ff6329df0b98358bc1539f6f4e
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-09-27 22:59:43 -0400

    first

---
+++ AccessingDataWithMySQL/src/main/java/com/adwms/AccessingDataWithMySqlApplication.java
@@ -0,0 +1,12 @@
+package com.adwms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataWithMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingDataWithMySqlApplication.class, args);
+	}
+}

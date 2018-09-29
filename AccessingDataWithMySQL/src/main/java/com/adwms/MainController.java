fc3667bcae8635ba2d0959a8c0e02573b3c60d65
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-09-28 21:10:36 -0400

    adjusted

--- AccessingDataWithMySQL/src/main/java/com/adwms/MainController.java
+++ AccessingDataWithMySQL/src/main/java/com/adwms/MainController.java
@@ -1,6 +1,5 @@
 package com.adwms;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
